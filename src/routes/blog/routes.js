45cd5499ffcfcdd8f1b40a2e0821bb30f612f7ee
TrustJimmy <r1ck54nchez@yandex.ru>
2024-03-06 12:53:19 +0300

    add 2+

--- src/routes/blog/routes.js
+++ src/routes/blog/routes.js
@@ -4,4 +4,9 @@ export const routes = [
         title:'My first blog post',
         body:'lorem ipsum dolor sit amet, coodkifio difoifdifod dsfhuisdbbsdi. dsifhuhdf dhsifu iuui sduiuibdf hsfiuhsfds,',
     },
+    {
+        id:2,
+        title:'My second blog post',
+        body:'lorem ipsum dolor sit amet, coodkifio difoifdifod dsfhuisdbbsdi. dsifhuhdf dhsifu iuui sduiuibdf hsfiuhsfds,',
+    }
 ];
