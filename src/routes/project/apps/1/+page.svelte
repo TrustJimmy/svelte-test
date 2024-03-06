2ecee53f88692332f315a1ba07f61985c4c64555
TrustJimmy <r1ck54nchez@yandex.ru>
2024-03-06 13:12:50 +0300

    Update +page.svelte

--- src/routes/project/apps/1/+page.svelte
+++ src/routes/project/apps/1/+page.svelte
@@ -19,6 +19,14 @@
         <input bind:value={newItem} placeholder="Enter to-do"/>
         <button class="add-todo" on:click={add}><span>+</span></button>
     </form>
+
+    <div class="todos">
+        {#each todoList as item, index}
+            <span class="todo_text">{item.task}</span>
+            <div class="todo_buttons"></div>
+        {/each}
+    </div>
 </main>
+<h1>My to-do list</h1>
 <style>
 </style>
