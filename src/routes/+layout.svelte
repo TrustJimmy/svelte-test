d6fa2a6982000e1ba62c56e969f4c075fc641015
TrustJimmy <r1ck54nchez@yandex.ru>
2024-03-06 09:57:53 +0300

    upd

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,9 @@
     <div class="nav-container">
         <a href="/" class="nav-logo" title="Back to Homepage">Ксения Пичугина</a>
         <div class="nav-links">
-            {#each nav as link}
+            {#if link.isShow}
             <a href={link.href} class="link">{link.title}</a>
-            {/each}
+            {/if}
         </div>
     </div>
 </nav>
@@ -18,19 +18,23 @@
     const nav = [
         {
             title:'Обо мне',
-            href:'/about'
+            href:'/about',
+            isShow: true,
         },
         {
             title: 'Блог',
-            href:'/blog'
+            href:'/blog',
+            isShow: true,
         },
         {
             title: 'Портфолио',
-            href:'/project'
+            href:'/project',
+            isShow: true,
         },
         {
             title: 'Контакты',
-            href:'/contact'
+            href:'/contact',
+            isShow: true,
         }
     ]
 </script>
