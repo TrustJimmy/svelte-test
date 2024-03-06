e50bc937a9baf526a968686e47a043c4e5564c92
TrustJimmy <r1ck54nchez@yandex.ru>
2024-03-06 12:36:27 +0300

    upd 2?

---
+++ src/routes/blog/routes.js
@@ -0,0 +1,7 @@
+export const routes = [
+    {
+        id:1,
+        title:'My first blog post',
+        body:'lorem ipsum dolor sit amet, coodkifio difoifdifod dsfhuisdbbsdi. dsifhuhdf dhsifu iuui sduiuibdf hsfiuhsfds,',
+    },
+];
