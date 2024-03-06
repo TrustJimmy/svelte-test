7a03ec777537a8d24a9cad06848fbc663a48200e
TrustJimmy <r1ck54nchez@yandex.ru>
2024-03-06 10:28:36 +0300

    -style

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -20,21 +20,3 @@
     import { routes } from './routes'
 </script>
 
-<style>
-    .container {
-        margin: 50px auto;
-        max-width: 800px;
-        padding: 0 20px;
-    }
-    .blogposts {
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        grid-gap: 20px;
-    }
-    .post {
-        border: 1px solid #ddd;
-        padding: 10px;
-        box-shadow: 0 0 10px #eee;
-    }
-    
-</style>
